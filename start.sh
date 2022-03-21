5d26c8273e476c4a392cadd0d81d0b297f0b3394
Tanujairam <88611197+Tanujairam123@users.noreply.github.com>
2022-03-21 17:31:44 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 echo "Cloning Repo, Please Wait..."
-git clone -b master https://github.com/UNKNOWN8884/massilama.git /massilama
+git clone -b master https://github.com/Tanujairam123/Filter-bot.git /Filter-bot
 cd /massilama
 echo "Installing Requirements..."
 pip3 install -U -r requirements.txt
